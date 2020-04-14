33e3a5b3c9b97909d9c3eef753c1bcc7e58a7a1c
cwllight8 <cwllight8@gmail.com>
2020-04-14 10:28:17 -0600

    made the outline of the weapon classes

---
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+public class Weapon : MonoBehaviour
+{
+
+    public string Name { get; set; }
+    public string Discription { get; set; }
+    public Image WeaponImage { get; set; }
+    public float Damage { get; set; }
+    public float AttackSpeed { get; set; }
+    public string TargetTag { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
