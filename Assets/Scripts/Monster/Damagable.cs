cddd7a028d7d3da585e4e0dbf24542e00074e783
Jason Clarke <jclarke@student.neumont.edu>
2020-04-15 16:49:53 -0600

    monster movement

---
+++ Assets/Scripts/Monster/Damagable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damagable : MonoBehaviour {
+
+[SerializeField] float m_health = 100;
+[SerializeField] Damage m_damage = null;
+[SerializeField] [Range(-1,1)]float m_damageReduction = 0;
+
+public float health { get => m_health; set => m_health = value; }
+public bool destroyed { get; set; } = false;
+public void ApplyDamage(float damageAmount) {
+	health = health - (damageAmount - (damageAmount*m_damageReduction));
+	if (!destroyed && health <= 0) {
+	Destroy(gameObject);
+	destroyed = true;
+
+	if (m_damage != null) {
+	Damage damage = Instantiate(m_damage, transform.position, Quaternion.identity);
+	damage.Spawn(transform.position, Vector3.zero, Vector3.up);
+	}
+}
+	
+}
+
+}
