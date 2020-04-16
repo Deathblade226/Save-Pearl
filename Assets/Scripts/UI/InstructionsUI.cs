0c80bb66dae0a9b1d96faa8351ada0db821ce931
Skota18855 <47341526+Skota18855@users.noreply.github.com>
2020-04-16 11:04:25 -0600

    Added title scene with my stuff

--- Assets/Scripts/UI/InstructionsUI.cs
+++ Assets/Scripts/UI/InstructionsUI.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class InstructionsUI : MonoBehaviour
 {
-    [SerializeField] Canvas Instructions = null;
+    [SerializeField] Canvas[] Instructions = null;
+    private int CurrentPage { get; set; }
 
     public void ChangePage(int pageNumber)
     {
-        Instructions.enabled = true;
+        Instructions[pageNumber].enabled = true;
+        Instructions[CurrentPage].enabled = false;
+        CurrentPage = pageNumber;
     }
 
     public void CloseInstructions()
     {
-        Instructions.enabled = false;
+        Instructions[CurrentPage].enabled = false;
     }
 }
