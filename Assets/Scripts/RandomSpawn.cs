89f37cbb49d7c6e9ad119724902e700b40715255
Jason Clarke <jclarke@student.neumont.edu>
2020-04-16 08:19:21 -0600

    spawning

---
+++ Assets/Scripts/RandomSpawn.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class RandomSpawn : MonoBehaviour {
+
+[SerializeField] List<GameObject> Spawned;
+
+private GameObject SpawnedItem = null;
+
+void Start() {
+    SpawnedItem = Spawned.ElementAt(Random.Range(0, Spawned.Count));
+    GameObject.Instantiate(SpawnedItem, gameObject.transform, false);
+}
+
+}
