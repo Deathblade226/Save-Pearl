f79df486e851099c1fe879cbdd9748aaa2239ecc
Jason Clarke <jclarke@student.neumont.edu>
2020-04-17 16:54:38 -0600

    dungeon generation

---
+++ Assets/Scripts/RuntimeSetup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DunGen;
+
+public class RuntimeSetup : MonoBehaviour {
+
+[SerializeField] DunGen.RuntimeDungeon dungeon = null;
+[SerializeField] [Range(0,2)]int DebugDifficulty = 0;
+[SerializeField] List<DunGen.Graph.DungeonFlow> difficulty = new List<DunGen.Graph.DungeonFlow>();
+
+private void Awake() {
+    dungeon.Generator.DungeonFlow = difficulty[DebugDifficulty];
+    dungeon.Generator.LengthMultiplier = 5.0f;
+}
+private void Start() {
+    dungeon.Generate();
+}
+
+}
