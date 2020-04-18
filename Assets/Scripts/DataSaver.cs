dd6c2ef413c2097896c55a177a51da1209a24e41
Skota18855 <47341526+Skota18855@users.noreply.github.com>
2020-04-18 16:38:20 -0600

    Added progress and player to be saved

--- Assets/Scripts/DataSaver.cs
+++ Assets/Scripts/DataSaver.cs
@@ -22,6 +22,22 @@ public class DataSaver
         set { difficulty = value; }
     }
 
+    private int progress;
+
+    public int Progress
+    {
+        get { return progress; }
+        set { progress = value; }
+    }
+
+    //private Player player;
+
+    //public int Player
+    //{
+    //    get { return player; }
+    //    set { player = value; }
+    //}
+
     public DataSaver(string name, int difficulty)
     {
         Name = name;
