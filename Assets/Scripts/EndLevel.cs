953e16b8dc1244c75f7147e9fe021b5dc8e974fb
Jason Clarke <jclarke@student.neumont.edu>
2020-04-20 16:54:34 -0600

    weapon spawner

---
+++ Assets/Scripts/EndLevel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndLevel : MonoBehaviour {
+[SerializeField] float range = 5.0f;
+[SerializeField] bool Debug = false;
+private void Update() {
+    GameObject player = AIUtilities.GetNearestGameObject(gameObject, "Player", range, 180.0f, true);
+    if (player != null) {
+    Game.Instance.Data.Progress++;
+    SceneManager.LoadScene("Game");
+    }    
+}
+void OnDrawGizmosSelected() {
+    // Draw a yellow sphere at the transform's position
+    if (Debug) { 
+    Gizmos.color = Color.yellow;
+    Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
+
+}
