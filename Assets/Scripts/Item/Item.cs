4ead2321652ab08255f85a66695b9e48b23b53df
Jason Clarke <jclarke@student.neumont.edu>
2020-04-19 19:06:02 -0600

    base item script

---
+++ Assets/Scripts/Item/Item.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+
+[SerializeField] float constitution = 0;
+[SerializeField] float strength = 0;
+[SerializeField] float dexterity = 0;
+[SerializeField] float jump = 0;
+[SerializeField] float speed = 0;
+[SerializeField] float armor = 0;
+
+private void OnTriggerEnter(Collider other) {
+        
+}
+
+}
