c2bee01071fd24c625c615f1fbb8c76b8e7522a2
Jason Clarke <jclarke@student.neumont.edu>
2020-04-19 20:51:34 -0600

    cleaning up and adding items

--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -12,6 +12,7 @@ public class Item : MonoBehaviour {
 [SerializeField] float armor = 0;
 
 private void OnTriggerEnter(Collider other) {
+
     if (other.gameObject.tag == "Player") { 
 
     Player player = other.gameObject.GetComponent<Player>();
